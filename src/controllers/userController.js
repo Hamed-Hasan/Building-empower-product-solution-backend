db56cd8ec5c3d7c433cd9e67cffb7f82b67bf21b
Hamed Hasan <hamedhasan422421@gmail.com>
2023-11-27 01:49:43 +0300

    🚀 Implemented user retrieval in getAllUsers function. 📊 Added functionality to create a new user in createUser. 🌟 Enhanced getUserById to retrieve a specific user by ID. 🏆 Updated updateUser to modify user details by ID. 🔄 Implemented deleteUser to remove a user by ID.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,48 +1,93 @@
 const userService = require('../services/userService');
 
 // Controller functions
+
+/**
+ * Get all users.
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ */
 const getAllUsers = async (req, res) => {
   try {
     const users = await userService.getAllUsers();
-    res.json(users);
+    res.status(200).json({
+      message: 'Users retrieved successfully',
+      status: 'success',
+      data: users,
+    });
   } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ status: 'error', error: 'Internal Server Error' });
   }
 };
 
+/**
+ * Create a new user.
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ */
 const createUser = async (req, res) => {
   try {
     const newUser = await userService.createUser(req.body);
-    res.status(201).json(newUser);
+    res.status(201).json({
+      message: 'User created successfully',
+      status: 'success',
+      data: newUser,
+    });
   } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ status: 'error', error: 'Internal Server Error' });
   }
 };
 
+/**
+ * Get a specific user by ID.
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ */
 const getUserById = async (req, res) => {
   try {
     const user = await userService.getUserById(req.params.userId);
-    res.json(user);
+    res.status(200).json({
+      message: 'User retrieved successfully',
+      status: 'success',
+      data: user,
+    });
   } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ status: 'error', error: 'Internal Server Error' });
   }
 };
 
+/**
+ * Update a user by ID.
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ */
 const updateUser = async (req, res) => {
   try {
     const updatedUser = await userService.updateUser(req.params.userId, req.body);
-    res.json(updatedUser);
+    res.status(200).json({
+      message: 'User updated successfully',
+      status: 'success',
+      data: updatedUser,
+    });
   } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ status: 'error', error: 'Internal Server Error' });
   }
 };
 
+/**
+ * Delete a user by ID.
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ */
 const deleteUser = async (req, res) => {
   try {
     await userService.deleteUser(req.params.userId);
-    res.json({ message: 'User deleted successfully' });
+    res.status(200).json({
+      message: 'User deleted successfully',
+      status: 'success',
+    });
   } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ status: 'error', error: 'Internal Server Error' });
   }
 };
 
