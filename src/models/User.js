05079d9b491e9129555582c293f4cf83ddc84c81
Hamed Hasan <hamedhasan422421@gmail.com>
2023-11-27 01:24:58 +0300

    🔧 Refactored User model structure for better readability. 📝 Updated User schema to include creation timestamp. 🔒 Enhanced password security in User model. 👤 Implemented user data validation in User schema. 🔄 Restructured User model for improved maintainability.

---
+++ src/models/User.js
@@ -0,0 +1,29 @@
+// models/User.js
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    created_at: {
+      type: Date,
+      default: Date.now,
+    },
+    created_by: {
+      type: String,
+      required: true,
+    },
+  });
+  
+  const User = mongoose.model('User', UserSchema);
+  
+  module.exports = User;
